1b2333145c76bb5dbac6c069d9ef20f158e7b2bd
sanmofe <58237454+sanmofe@users.noreply.github.com>
2024-03-23 02:45:15 -0500

    Santiago (#4)
    * Se hicieron modificaciones para que se muestre una primera versión del login
    
    * Se movió el login a su propio archivo y se creó la vista del mapa
    
    * Se inició el tomar la localización del teléfono en mapView
    
    * Se corrigió un merge conflict

--- lib/mapView.dart
+++ lib/mapView.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -19,12 +19,48 @@ class _MapViewState extends State<MapView> {
 
   static const LatLng _center = LatLng(4.6029286, -74.0653713);
 
+  Future<Position> getUserCurrentLocation() async {
+
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+ if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Center(child: Text("SEXOOOOO")),
+        title: const Center(child: Text("U. De Los Andes")),
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.menu),
@@ -41,12 +77,24 @@ class _MapViewState extends State<MapView> {
           ),
         ],
       ),
-      body: const Center(
+      body:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
               child: GoogleMap( 
+                onMapCreated: (GoogleMapController controller){
+                  _controller.complete(controller);
+                  () async {
+                    final Position position = await getUserCurrentLocation();
+                    controller.animateCamera(CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: LatLng(position.latitude, position.longitude),
+                        zoom: 19.0,
+                      ),
+                    ));
+                  };
+                },
                 initialCameraPosition: CameraPosition(
                   target: _center,
                   zoom: 19.0,
@@ -59,4 +107,6 @@ class _MapViewState extends State<MapView> {
     );
   }
 }
+
+
 
