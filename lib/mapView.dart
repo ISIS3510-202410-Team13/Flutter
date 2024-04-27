5cdb094434b3dcb1334c83abef805fa1e1cdba3c
sanmofe <58237454+sanmofe@users.noreply.github.com>
2024-04-26 22:00:00 -0500

    Santiago (#15)
    * Se hicieron modificaciones para que se muestre una primera versión del login
    
    * Se movió el login a su propio archivo y se creó la vista del mapa
    
    * Se inició el tomar la localización del teléfono en mapView
    
    * Se corrigió un merge conflict
    
    * Añadidos cambios para que el mapa se abra en la posición del restaurante
    
    * Se corrigió un error de las latitudes y longitudes
    
    * Se avanzó en la implementación de la ruta y el caché
    
    * Se hicieron cambios a la lógica de la caché

--- lib/mapView.dart
+++ lib/mapView.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
 
 class MapView extends StatefulWidget {
   const MapView({
@@ -24,10 +26,15 @@ class _MapViewState extends State<MapView> {
 
   LatLng? _center;
 
+  Map<PolylineId, Polyline> polylines = {};
+
+  late Future<Position> _currentPosition;
+
   @override
   void initState() {
     super.initState();
     _center = LatLng(widget.lat, widget.long);
+    _currentPosition = getUserCurrentLocation();
   }
 
   Future<Position> getUserCurrentLocation() async {
@@ -54,15 +61,70 @@ class _MapViewState extends State<MapView> {
       }
     }
 
+
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
+  }
+
+    Future<List<LatLng>> fetchPolylinePoints() async {
+    final polylinePoints = PolylinePoints();
+
+    final result = await polylinePoints.getRouteBetweenCoordinates(
+      "AIzaSyAl1qbz6VmRsbbc5bbMUZAvUeNWRG9ofic",
+      PointLatLng(4.6025061, -74.066107),
+      PointLatLng(widget.lat, widget.long),
+    );
+
+    if (result.points.isNotEmpty) {
+      return result.points
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
+    } else {
+      debugPrint(result.errorMessage);
+      return [];
+    }
+  }
+
+  Future<void> generatePolyLineFromPoints(
+      List<LatLng> polylineCoordinates) async {
+    const id = PolylineId('polyline');
+
+    final polyline = Polyline(
+      polylineId: id,
+      color: Colors.blueAccent,
+      points: polylineCoordinates,
+      width: 5,
+    );
+
+    setState(() => polylines[id] = polyline);
+
+    print(polylines);
+    }
+
+  getPolypoints(){
+    //Position current = _currentPosition as Position;
+    LatLng desde = LatLng(4.6025061, -74.066107);
+    LatLng hasta = LatLng(widget.lat, widget.long);
+
+    final Set<Polyline> _polyline = {};
+
+    _polyline.add( 
+      Polyline(
+        polylineId: PolylineId('ruta'),
+        visible: true,
+        points: [desde, hasta],
+        color: Colors.blue,
+        width: 4,
+      )
+    );
+
+  return _polyline;
   }
 
   @override
@@ -93,12 +155,13 @@ class _MapViewState extends State<MapView> {
           children: <Widget>[
             Expanded(
               child: GoogleMap(
+             polylines: polylines.values.toSet(),
                 myLocationEnabled: true,
                 myLocationButtonEnabled: true,
                 onMapCreated: (GoogleMapController controller) {
                   _controller.complete(controller);
                   () async {
-                    final Position position = LatLng(0, 0) as Position;
+                    final Position position = getUserCurrentLocation() as Position;
                     controller.animateCamera(CameraUpdate.newCameraPosition(
                       CameraPosition(
                         target: LatLng(position.latitude, position.longitude),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import connectivity_plus_macos
 import firebase_auth
 import firebase_core
 import firebase_database
@@ -13,9 +14,11 @@ import firebase_database
 import geolocator_apple
 import google_sign_in_ios
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
@@ -23,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
